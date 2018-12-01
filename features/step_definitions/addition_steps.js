835437268ae1ee2d47f2128cfaaf7ca41a212c44
Daniel Medeiros <danielmmf@gmail.com>
2018-12-01 02:04:52 -0200

    initial commit

---
+++ features/step_definitions/addition_steps.js
@@ -0,0 +1,21 @@
+const { Before, Given, When, Then } = require('cucumber');
+const Calculator = require('../../lib/calculator');
+
+let calculator;
+
+	Given('the numbers {int} and {int}', function (int, int2) {
+       // Write code here that turns the phrase above into concrete actions
+       
+     });
+   
+
+     When('they are added together', function () {
+       // Write code here that turns the phrase above into concrete actions
+       return 'pending';
+     });
+   
+
+     Then('should the result be {int}', function (int) {
+       // Write code here that turns the phrase above into concrete actions
+       return 'pending';
+     });
