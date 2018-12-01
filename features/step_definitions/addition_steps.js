a64b9d1dcb3f196f1d1feedc7a7bf2608d9da4be
Daniel Medeiros <danielmmf@gmail.com>
2018-12-01 02:12:31 -0200

    passando os testes

--- features/step_definitions/addition_steps.js
+++ features/step_definitions/addition_steps.js
@@ -1,21 +1,17 @@
+const assert = require('assert')
 const { Before, Given, When, Then } = require('cucumber');
 const Calculator = require('../../lib/calculator');
 
 let calculator;
 
 	Given('the numbers {int} and {int}', function (int, int2) {
-       // Write code here that turns the phrase above into concrete actions
-       
-     });
-   
+       calculator = new Calculator(int, int2);
+	});
 
-     When('they are added together', function () {
-       // Write code here that turns the phrase above into concrete actions
-       return 'pending';
-     });
-   
+	When('they are added together', function () {
+	    calculator.add();
+	});
 
-     Then('should the result be {int}', function (int) {
-       // Write code here that turns the phrase above into concrete actions
-       return 'pending';
-     });
+	Then('should the result be {int}', function (expected) {
+	    assert.equal(calculator.getResult(), expected)
+    });
